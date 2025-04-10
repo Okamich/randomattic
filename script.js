ad7145361b4fd456a1e49eb1490f8d6ff633db92
Okamich <sporer1@gmail.com>
2025-04-10 14:13:05 +0300

    Update script.js
    working

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 const names = ['Артур','Эльза','Гарольд','Лиана','Борис','Мира','Дариус','Селена'];
-const generateBtn = document.getElementById('generate-btn');
-const result = document.getElementById('result');
 
-generateBtn.addEventListener('click', () => {
-  console.log('Button pressed');
-  const randomIndex = Math.floor(Math.random() * names.lenght);
-  result.textContent = names[randomIndex];
-});
+    function generateName() {
+      console.log('Имена:', names)
+      console.log('длинна массива:', names.length)
+      const randomIndex = Math.floor(Math.random()*names.length);
+      console.log('Выбран индекс:', randomIndex)
+      console.log('Сгенерировано имя:', names[randomIndex])
+      return names[randomIndex];
+    }
+    
+    document.getElementById('generate-btn').addEventListener('click', function() {
+      console.log('Button pressed')
+      const nameOutput = document.getElementById('name-output');
+      if (nameOutput) {
+        nameOutput.textContent = generateName();
+      } else {
+        console.error('Элемент с id "name-output" не найден');
+      }
+    });
